1f13cd5a84e617402e1381a197781f8a707e60cf
Chew Chee Wai <chew.cheewai@ynytechnology.com.my>
2023-05-12 10:29:50 +0800

    almost never need to use nvim-colorizer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,20 +27,20 @@ local default_plugins = {
 		end,
 	},
 
-	{
-		"NvChad/nvim-colorizer.lua",
-		init = function()
-			require("core.utils").lazy_load("nvim-colorizer.lua")
-		end,
-		config = function(_, opts)
-			require("colorizer").setup(opts)
+	-- {
+	-- 	"NvChad/nvim-colorizer.lua",
+	-- 	init = function()
+	-- 		require("core.utils").lazy_load("nvim-colorizer.lua")
+	-- 	end,
+	-- 	config = function(_, opts)
+	-- 		require("colorizer").setup(opts)
 
-			-- execute colorizer as soon as possible
-			vim.defer_fn(function()
-				require("colorizer").attach_to_buffer(0)
-			end, 0)
-		end,
-	},
+	-- 		-- execute colorizer as soon as possible
+	-- 		vim.defer_fn(function()
+	-- 			require("colorizer").attach_to_buffer(0)
+	-- 		end, 0)
+	-- 	end,
+	-- },
 
 	{
 		"nvim-tree/nvim-web-devicons",
