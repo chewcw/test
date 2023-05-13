c798355ddf9fa17dbda68d281ed5258d4292eb60
Chew Chee Wai <chew.cheewai@ynytechnology.com.my>
2023-05-13 21:49:10 +0800

    missed one

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.config/nvim
 git clone https://github.com/chewcw/test.git ~/.config/nvim || true
 
 sudo apt-get update
-sudo apt-get install -y nodejs npm ripgrep build-essential xclip xsel
+sudo apt-get install -y nodejs npm ripgrep build-essential xclip xsel python3-venv
 wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb -O /tmp/nvim-linux64.deb
 sudo dpkg -i /tmp/nvim-linux64.deb
 
